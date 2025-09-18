efbc1e5260c00daf03863855dbee5fbad5b2c38e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-17 22:08:28 -0300

    refactor: add main.js file

---
+++ main.js
@@ -0,0 +1,38 @@
+
+        function getHoras(){
+            inputHoras = document.getElementById("HoraInicial")
+            inputHorasFim = document.getElementById("HoraFinal")
+            inputMinutos = document.getElementById("MinutoInicial")
+            inputMinutosFim = document.getElementById("MinutoFinal")
+
+            totalInicial = (inputHoras.value * 60) + parseInt(inputMinutos.value)
+
+            totalFinal = ((inputHorasFim.value * 60) + parseInt(inputMinutosFim.value))
+
+            totalDeMinutos = totalFinal - totalInicial
+
+            restoMinutos = totalDeMinutos % 60
+
+            totalHoras = (totalDeMinutos - restoMinutos) / 60
+            console.log(`As horas são ${totalHoras} e os minutos são ${restoMinutos}`)
+
+            tempoTotal = document.getElementById("tempoTotal")
+
+            if (totalHoras < 0){
+                totalHoras = totalHoras * -1
+            }
+            if (restoMinutos < 0){
+                restoMinutos = restoMinutos * -1
+            }
+            if (totalHoras < 10){
+                console.log(totalHoras)
+                totalHoras = "0" + totalHoras
+            }
+
+            if (restoMinutos < 10){
+                restoMinutos = "0" + restoMinutos
+            }
+            tempoTotal.innerText = `Duracao: ${totalHoras}:${restoMinutos}`
+
+            console.log("ULTIMO PRINT". totalHoras, restoMinutos)
+        }
